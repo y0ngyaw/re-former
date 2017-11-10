a6b33b8f208007dc0d0c56f608f25b94d267e009
y0ngyaw <yongyawyeo98@gmail.com>
2017-11-10 14:55:40 +0800

    Complete the project including error messages

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	validates :username, presence: true, length: { maximum:50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+	validates :email, presence: true, length: { maximum: 255 }, 
+				format: { with: VALID_EMAIL_REGEX }
+	validates :password, presence: true, length: { minimum: 6 }
+
+end
